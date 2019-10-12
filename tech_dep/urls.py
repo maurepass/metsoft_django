b02a66a33254f40ab5aaddfd3adad6035c0ae8d8
maurepass <maurepass@gmail.com>
2019-10-12 22:13:06 +0200

    Replaced function view by class view in tech_dep

--- tech_dep/urls.py
+++ tech_dep/urls.py
@@ -8,6 +8,6 @@ router.register(r'orders', views.OrderViewSet, 'orders')
 
 urlpatterns = [
     re_path('^api/', include(router.urls)),
-    path('orders/', views.orders, name='orders-index'),
+    path('orders/', views.OrdersView.as_view(), name='orders-index'),
     path('<int:pk>/update/', views.OrderUpdateView.as_view(), name='order-update'),
 ]
--- tech_dep/views.py
+++ tech_dep/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
-from django.shortcuts import render
-from django.views.generic import UpdateView
+from django.views.generic import UpdateView, TemplateView
 from rest_framework import viewsets
 
 from prod_reports.models import Pocastord
@@ -10,33 +9,36 @@ from .models import Order
 from .serializers import OrderSerializers
 
 
-def orders(request):
-    last_poc_kokila = Pocastord.objects.using('kokila').last()
-    last_poc_orders = Order.objects.last()
+class OrdersView(TemplateView):
+    template_name = 'tech_dep/orders.html'
 
-    if last_poc_kokila.pk > last_poc_orders.id_poc:
-        items = Pocastord.objects.using('kokila').filter(pk__gt=last_poc_orders.id_poc - 50)
-        for item in items:
-            Order.objects.update_or_create(
-                id_poc=item.id,
-                defaults={
-                    'numer_met': item.porder.met_no,
-                    'company': item.porder.zamawiajacy.company,
-                    'cast_name': item.cast_name,
-                    'cast_pcs': item.cast_pcs,
-                    'pict_number': item.pict_number,
-                    'cust_material': item.cust_material,
-                    'termin_klienta': item.porder.customer_date,
-                    'model': item.model,
-                    'rawcast': item.rawcast,
-                    'painting': item.painting,
-                    'mechrough': item.mechrough,
-                    'mechfine': item.mechfine,
-                    'marketing': item.porder.wprowadzajacy,
-                    'ord_in': item.created_at,
-                }
-            )
-    return render(request, 'tech_dep/orders.html')
+    def dispatch(self, request, *args, **kwargs):
+        last_poc_kokila = Pocastord.objects.using('kokila').last()
+        last_poc_orders = Order.objects.last()
+
+        if last_poc_kokila.pk > last_poc_orders.id_poc:
+            items = Pocastord.objects.using('kokila').filter(pk__gt=last_poc_orders.id_poc - 50)
+            for item in items:
+                Order.objects.update_or_create(
+                    id_poc=item.id,
+                    defaults={
+                        'numer_met': item.porder.met_no,
+                        'company': item.porder.zamawiajacy.company,
+                        'cast_name': item.cast_name,
+                        'cast_pcs': item.cast_pcs,
+                        'pict_number': item.pict_number,
+                        'cust_material': item.cust_material,
+                        'termin_klienta': item.porder.customer_date,
+                        'model': item.model,
+                        'rawcast': item.rawcast,
+                        'painting': item.painting,
+                        'mechrough': item.mechrough,
+                        'mechfine': item.mechfine,
+                        'marketing': item.porder.wprowadzajacy,
+                        'ord_in': item.created_at,
+                    }
+                )
+        return super().dispatch(request, *args, **kwargs)
 
 
 class OrderViewSet(viewsets.ModelViewSet):
