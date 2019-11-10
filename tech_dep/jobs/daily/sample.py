4ee5616ca0d1c92c7fb1a68933c6037243ff2914
maurepass <maurepass@gmail.com>
2019-11-10 09:46:19 +0100

    Added css formatting to mail message and code refactoring.

--- tech_dep/jobs/daily/sample.py
+++ tech_dep/jobs/daily/sample.py
@@ -1,9 +1,28 @@
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from django_extensions.management.jobs import DailyJob
 
-from tech_dep.views import daily_mail_tech_department
+
+from METsoft.settings import EMAIL_HOST_USER
+from offers.models import Offer
+from tech_dep.models import Order
 
 
 class Job(DailyJob):
+    """ Send mail with offers and orders in progress. """
 
     def execute(self):
-        daily_mail_tech_department()
+        context = {
+            'offers': Offer.objects.filter(status_id=1).order_by('-id'),
+            'orders': Order.objects.filter(status_id=2).order_by('-id'),
+        }
+
+        user = User.objects.get(pk=4)
+
+        send_mail('METsoft - daily mail',
+                  'Some message',
+                  from_email=EMAIL_HOST_USER,
+                  recipient_list=[user.email],
+                  html_message=render_to_string('tech_dep/daily_mail_tech_department.html', context)
+                  )
