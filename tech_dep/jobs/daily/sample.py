6484b257819e4437ac38e1f419cd74688a19c17b
maurepass <maurepass@gmail.com>
2019-11-10 12:36:01 +0100

    Added sending new nonconformities in daily mail

--- tech_dep/jobs/daily/sample.py
+++ tech_dep/jobs/daily/sample.py
@@ -1,21 +1,31 @@
+from datetime import timedelta, date
+
+from dateutil.utils import today
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django_extensions.management.jobs import DailyJob
 
-
 from METsoft.settings import EMAIL_HOST_USER
 from offers.models import Offer
+from prod_reports.models import Operation
 from tech_dep.models import Order
 
 
 class Job(DailyJob):
-    """ Send mail with offers and orders in progress. """
+    """ Send mail with offers and orders in progress and nonconformities from last day"""
 
     def execute(self):
+        # if is Monday send nonconformities since Friday
+        if date.isoweekday(today()) == 1:
+            completion_date = today() - timedelta(days=3)
+        else:
+            completion_date = today() - timedelta(days=1)
+
         context = {
             'offers': Offer.objects.filter(status_id=1).order_by('-id'),
             'orders': Order.objects.filter(status_id=2).order_by('-id'),
+            'nonconformities': Operation.objects.filter(accordance=3, completion_date1=completion_date)
         }
 
         user = User.objects.get(pk=4)
