80edeb6e1b5e1456168b634e1e4d81f878c2b3cd
maurepass <maurepass@gmail.com>
2019-11-19 08:45:18 +0100

    Added requried False in orders update form

--- tech_dep/forms.py
+++ tech_dep/forms.py
@@ -4,14 +4,14 @@ from .models import Order
 
 
 class OrderUpdateForm(forms.ModelForm):
-    numer_met = forms.CharField(disabled=True, label='Nr zlecenia')
-    company = forms.CharField(disabled=True, label='Klient')
-    cast_name = forms.CharField(disabled=True, label='Nazwa odlewu')
-    pict_number = forms.CharField(disabled=True, label='Nr rysunku')
-    cast_pcs = forms.IntegerField(disabled=True, label='Il. sztuk')
-    cust_material = forms.CharField(disabled=True, label='Materiał')
-    marketing = forms.CharField(disabled=True, label='Marketing')
-    ord_in = forms.DateField(disabled=True, label='Data otrzymania')
+    numer_met = forms.CharField(disabled=True, required=False, label='Nr zlecenia')
+    company = forms.CharField(disabled=True, required=False, label='Klient')
+    cast_name = forms.CharField(disabled=True, required=False, label='Nazwa odlewu')
+    pict_number = forms.CharField(disabled=True, required=False, label='Nr rysunku')
+    cast_pcs = forms.IntegerField(disabled=True, required=False, label='Il. sztuk')
+    cust_material = forms.CharField(disabled=True, required=False, label='Materiał')
+    marketing = forms.CharField(disabled=True, required=False, label='Marketing')
+    ord_in = forms.DateField(disabled=True, required=False, label='Data otrzymania')
 
     class Meta:
         model = Order
