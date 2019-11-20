f9c02ed621ebdf27b4a4305b1a79f0c97e2e2978
maurepass <maurepass@gmail.com>
2019-11-20 18:55:26 +0100

    Bug fix (unreadable id in some columns). Replaced thousand separator by intcomma.

--- METsoft/settings/base.py
+++ METsoft/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 
     # third part apps
     'rest_framework',
@@ -123,8 +124,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-USE_THOUSAND_SEPARATOR = True
 
 
 # Static files (CSS, JavaScript, Images)
